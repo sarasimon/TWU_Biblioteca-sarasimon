6294d78e679a7fcbacd20bc6f12d439ae045c301
sarasimonwillis-fleming <sswfleming@gmail.com>
2017-07-21 15:44:37 +0200

    Login singleton created

---
+++ src/test/java/LoginServiceTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginTest {
+
+    String libraryNumber = "123-4567";
+    String password = "1111";
+
+    @Test
+    public void testLoginIsCorrectWhenUsernameAndPasswordMatch(){
+        Login login = Login.getInstance();
+        login.
+    }
+
+}

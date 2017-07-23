814b65e87174006bde01bf95710ff2bd3665819e
sarasimonwillis-fleming <sswfleming@gmail.com>
2017-07-23 14:31:01 +0200

    Before refactor of Enum Actions

--- src/main/java/com/twu/biblioteca/action/CheckOutAction.java
+++ src/main/java/com/twu/biblioteca/action/CheckOutAction.java
@@ -3,27 +3,27 @@ package com.twu.biblioteca.action;
 import com.twu.biblioteca.StoreInterface;
 import com.twu.biblioteca.Input;
 
-public class CheckOutAction extends Action {
+public class CheckOutAction extends ActionWithInputAndStore {
 
     public CheckOutAction(Input input, StoreInterface store) {
         super(store, input);
     }
 
     public boolean go() {
-        System.out.println("Please, give the title of the book you want to check out:");
+        System.out.println("Please, give the " + store.element() + " you want to check out:");
         String title = input.read();
         if (isAbleToCheckOut(title)) {
             checkOutBook(title);
             return true;
         } else {
-            System.out.println("That book is not available. Please try again.\n");
+            System.out.println("That " + store.element() + " is not available. Please try again.\n");
             return true;
         }
     }
 
     private void checkOutBook(String title) {
         if (store.checkOut(title)) {
-            System.out.println("Thank you! Enjoy the book");
+            System.out.println("Thank you! Enjoy the " + store.element());
         }
     }
 
